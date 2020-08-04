e114ad36ff37b8fd6bcb05aeedf0f25baf16b0dc
Niko Sward <nicholascsward@gmail.com>
2020-08-04 09:35:06 -0500

    updated resume and centered text for home

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -8,21 +8,21 @@ const Home = () => {
         <h1>Howdy</h1>
         <div className="homeRow">
           <h3>
-            I am Niko, a full stack software engineer with professional
-            experience in education and music. Being a lifelong fan of solving
-            logic puzzles and having a history of building creative projects led
-            me to tackle Harvard's CS50 course while teaching music full time. I
-            impressed my developer friends with my work and they encouraged me
-            to apply to Fullstack Academy's immersive software engineering
-            program, where I pursued programming full-time.
+            <big>I'm Niko,</big> a full stack software engineer with
+            professional experience in education and music. Being a lifelong fan
+            of solving logic puzzles and having a history of building creative
+            projects led me to tackle Harvard's CS50 course while teaching music
+            full time. I impressed my developer friends with my work and they
+            encouraged me to apply to Fullstack Academy's immersive software
+            engineering program, where I pursued programming full-time.
           </h3>
           <h3>
-            Proficiencies: Node.js, Express, React, Databases using SQL,
-            JavaScript (ES6), HTML5/CSS, Redux, JSX, Git, AJAX, JSON, Tone.js,
-            Jasmine, Mocha, Chai, and various APIs.{' '}
+            <big>Proficiencies:</big> Node.js, Express, React, Databases using
+            SQL, JavaScript (ES6), HTML5/CSS, Redux, JSX, Git, AJAX, JSON,
+            Tone.js, Jasmine, Mocha, Chai, and various APIs.{' '}
           </h3>
           <h3>
-            Knowledgeable in: C, Python, Java, Swift, Firebase, AWS (EC2),
+            <big>Knowledgeable in:</big> C, Python, Swift, Firebase, AWS (EC2),
             Travis CI, Webpack, Babel, Socket.io and Raspberry Pi OS.
           </h3>
         </div>
