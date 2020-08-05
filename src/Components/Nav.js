a10844cad81bbf7ddc72e17c8669ea72b2e49bbe
Niko Sward <nicholascsward@gmail.com>
2020-08-05 17:55:04 -0500

    made custom dropdown menu for mobile

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,83 +1,67 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
-import { Dropdown } from 'react-bootstrap';
 
 class Navbar extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      mobileDropDisplay: false,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    this.setState((state) => ({
+      mobileDropDisplay: !state.mobileDropDisplay,
+    }));
   }
   render() {
     return (
-      <nav>
-        {/* <ul id="mobileNav">
-          <li>
-            <NavLink to="/">Niko Sward</NavLink>
-          </li>
-          <Dropdown>
-            <Dropdown.Toggle variant="success" id="dropdown-basic">
-              <img src={require('../images/burger.png')} alt="" />
-            </Dropdown.Toggle>
+      <section>
+        <nav>
+          <ul id="mobileNav">
+            <li>
+              <NavLink to="/">Niko Sward</NavLink>
+            </li>
 
-            <Dropdown.Menu>
-              <div>
-                <NavLink to="/projects">Projects</NavLink>
-              </div>
-              <div>
-                <NavLink to="/background">Background</NavLink>
-              </div>
-              <div>
-                <NavLink to="/resume">Resume</NavLink>
-              </div>
-              <div>
-                <NavLink to="/contact">Contact</NavLink>
-              </div>
-            </Dropdown.Menu>
-          </Dropdown>
-        </ul> */}
-        <ul id="mobileNav">
-          <li>
-            <NavLink to="/">Niko Sward</NavLink>
-          </li>
-          <Dropdown>
-            <Dropdown.Toggle variant="success" id="dropdown-basic">
+            <div onClick={this.handleClick}>
               <img src={require('../images/burger.png')} alt="" />
-            </Dropdown.Toggle>
-
-            <Dropdown.Menu>
-              <div>
-                <NavLink to="/projects">Projects</NavLink>
-              </div>
-              <div>
-                <NavLink to="/background">Background</NavLink>
-              </div>
-              <div>
-                <NavLink to="/resume">Resume</NavLink>
-              </div>
-              <div>
-                <NavLink to="/contact">Contact</NavLink>
-              </div>
-            </Dropdown.Menu>
-          </Dropdown>
-        </ul>
-        <ul id="fullNav">
-          <li>
-            <NavLink to="/">Niko Sward - Full Stack Web Developer</NavLink>
-          </li>
-          <li>
-            <NavLink to="/projects">Projects</NavLink>
-          </li>
-          <li>
-            <NavLink to="/background">Background</NavLink>
-          </li>
-          <li>
-            <NavLink to="/resume">Resume</NavLink>
-          </li>
-          <li>
-            <NavLink to="/contact">Contact</NavLink>
-          </li>
-        </ul>
-      </nav>
+            </div>
+          </ul>
+          <ul id="fullNav">
+            <li>
+              <NavLink to="/">Niko Sward - Full Stack Web Developer</NavLink>
+            </li>
+            <li>
+              <NavLink to="/projects">Projects</NavLink>
+            </li>
+            <li>
+              <NavLink to="/background">Background</NavLink>
+            </li>
+            <li>
+              <NavLink to="/resume">Resume</NavLink>
+            </li>
+            <li>
+              <NavLink to="/contact">Contact</NavLink>
+            </li>
+          </ul>
+        </nav>
+        {this.state.mobileDropDisplay && (
+          <section className="mobileDrop" onClick={this.handleClick}>
+            <div>
+              <NavLink to="/projects">Projects</NavLink>
+            </div>
+            <div>
+              <NavLink to="/background">Background</NavLink>
+            </div>
+            <div>
+              <NavLink to="/resume">Resume</NavLink>
+            </div>
+            <div>
+              <NavLink to="/contact">Contact</NavLink>
+            </div>
+          </section>
+        )}
+      </section>
     );
   }
 }
