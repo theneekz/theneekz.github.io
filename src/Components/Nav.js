89cc97387d026a055001464a3bec266b9d2a1f18
Niko Sward <nicholascsward@gmail.com>
2020-08-05 17:32:24 -0500

    meaningful kermit

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,54 +1,85 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import { Dropdown } from 'react-bootstrap';
 
-const Navbar = () => {
-  return (
-    <nav>
-      <ul id="mobileNav">
-        <li>
-          <NavLink to="/">Niko Sward</NavLink>
-        </li>
-        <Dropdown>
-          <Dropdown.Toggle variant="success" id="dropdown-basic">
-            <img src={require('../images/burger.png')} alt="" />
-          </Dropdown.Toggle>
+class Navbar extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+  render() {
+    return (
+      <nav>
+        {/* <ul id="mobileNav">
+          <li>
+            <NavLink to="/">Niko Sward</NavLink>
+          </li>
+          <Dropdown>
+            <Dropdown.Toggle variant="success" id="dropdown-basic">
+              <img src={require('../images/burger.png')} alt="" />
+            </Dropdown.Toggle>
 
-          <Dropdown.Menu>
-            <div>
-              <NavLink to="/projects">Projects</NavLink>
-            </div>
-            <div>
-              <NavLink to="/background">Background</NavLink>
-            </div>
-            <div>
-              <NavLink to="/resume">Resume</NavLink>
-            </div>
-            <div>
-              <NavLink to="/contact">Contact</NavLink>
-            </div>
-          </Dropdown.Menu>
-        </Dropdown>
-      </ul>
-      <ul id="fullNav">
-        <li>
-          <NavLink to="/">Niko Sward - Full Stack Web Developer</NavLink>
-        </li>
-        <li>
-          <NavLink to="/projects">Projects</NavLink>
-        </li>
-        <li>
-          <NavLink to="/background">Background</NavLink>
-        </li>
-        <li>
-          <NavLink to="/resume">Resume</NavLink>
-        </li>
-        <li>
-          <NavLink to="/contact">Contact</NavLink>
-        </li>
-      </ul>
-    </nav>
-  );
-};
+            <Dropdown.Menu>
+              <div>
+                <NavLink to="/projects">Projects</NavLink>
+              </div>
+              <div>
+                <NavLink to="/background">Background</NavLink>
+              </div>
+              <div>
+                <NavLink to="/resume">Resume</NavLink>
+              </div>
+              <div>
+                <NavLink to="/contact">Contact</NavLink>
+              </div>
+            </Dropdown.Menu>
+          </Dropdown>
+        </ul> */}
+        <ul id="mobileNav">
+          <li>
+            <NavLink to="/">Niko Sward</NavLink>
+          </li>
+          <Dropdown>
+            <Dropdown.Toggle variant="success" id="dropdown-basic">
+              <img src={require('../images/burger.png')} alt="" />
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              <div>
+                <NavLink to="/projects">Projects</NavLink>
+              </div>
+              <div>
+                <NavLink to="/background">Background</NavLink>
+              </div>
+              <div>
+                <NavLink to="/resume">Resume</NavLink>
+              </div>
+              <div>
+                <NavLink to="/contact">Contact</NavLink>
+              </div>
+            </Dropdown.Menu>
+          </Dropdown>
+        </ul>
+        <ul id="fullNav">
+          <li>
+            <NavLink to="/">Niko Sward - Full Stack Web Developer</NavLink>
+          </li>
+          <li>
+            <NavLink to="/projects">Projects</NavLink>
+          </li>
+          <li>
+            <NavLink to="/background">Background</NavLink>
+          </li>
+          <li>
+            <NavLink to="/resume">Resume</NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact">Contact</NavLink>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+}
 
 export default Navbar;
