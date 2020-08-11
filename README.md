3dd052b3084e37915f36ef6a219fc4ee9b74119a
Niko Sward <nicholascsward@gmail.com>
2020-08-11 14:19:43 -0500

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-# Thanks for coming to my portfolio!
-
-This app was bootstrapped by create-react-app and stylized by me. If you have any questions, email me at nicholascsward@gmail.com.
---
+++ precache-manifest.9384e0b190e1b93b230db6ce96373800.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc33630fea2722468ca7b4dc36910127",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad3e6bbb43cf6a8bc55a",
+    "url": "/static/css/main.d1b932b5.chunk.css"
+  },
+  {
+    "revision": "5f947a756212aa595b0e",
+    "url": "/static/js/2.46bb0bbd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.46bb0bbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad3e6bbb43cf6a8bc55a",
+    "url": "/static/js/main.bce4abf1.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "efc909b8680823260acd00f7bdc251f7",
+    "url": "/static/media/DMCE_small.efc909b8.png"
+  },
+  {
+    "revision": "63ed841be62fb8ef25cc7a884f0e7bc8",
+    "url": "/static/media/FPthumb_small.63ed841b.png"
+  },
+  {
+    "revision": "f3c1e94e09feec1ce6ebcf3b001d9cc2",
+    "url": "/static/media/Finance_small.f3c1e94e.png"
+  },
+  {
+    "revision": "5bf7aefdedcc4a26c50c39373c51877e",
+    "url": "/static/media/GSthumb_small.5bf7aefd.png"
+  },
+  {
+    "revision": "f34b63847983797eebe07ff86059320e",
+    "url": "/static/media/Resume.f34b6384.jpg"
+  },
+  {
+    "revision": "ac74c398f82b3ad846d50b02463c4d86",
+    "url": "/static/media/SiqBeets_small.ac74c398.png"
+  },
+  {
+    "revision": "114165ecbb2d2cc52049f27cb2b2fb70",
+    "url": "/static/media/george.114165ec.png"
+  },
+  {
+    "revision": "3c0f81500c1e39a58d9bec641d35b0d6",
+    "url": "/static/media/neeks_george.3c0f8150.png"
+  },
+  {
+    "revision": "d2be2c8dfee1fddaf09259ed9001061b",
+    "url": "/static/media/speller_small.d2be2c8d.png"
+  }
+]);
